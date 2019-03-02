b955ae1c36ea90ae0f0777e7f02de46918b7a252
sbathgate <sbathgate03@gmail.com>
2019-03-02 13:50:24 -0800

    tmp

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ import json
 import os
 import random
 import bottle
+import math
 
 from api import ping_response, start_response, move_response, end_response
 
@@ -17,7 +18,6 @@ def static(path):
     """
     Given a path, return the static file located relative
     to the static folder.
-
     This can be used to return the snake head URL in an API response.
     """
     return bottle.static_file(path, root='static/')
@@ -32,7 +32,10 @@ def ping():
 
 @bottle.post('/start')
 def start():
+    global boardWidth, boardHeight
+
     data = bottle.request.json
+
 
     """
     TODO: If you intend to have a stateful snake AI,
@@ -41,23 +44,111 @@ def start():
     """
     print(json.dumps(data))
 
-    color = "#00FF00"
+    boardWidth = data['board']['width']
+    boardHeight = data['board']['height']
+    gameID = data['game']['id']
+    color = "#50C878"
 
     return start_response(color)
 
 
+def getMyLocation(data):
+  return data['you']['body'][0]
+
+def getAdjacentCells(myLocation, data):
+    # Coordinates of mySnake head and adajacent spaces
+    adjacent = {}
+
+    adjacent['up'] = [myLocation['y']-1, myLocation['x']]
+    adjacent['down'] = [myLocation['y']+1, myLocation['x']]
+    adjacent['left'] = [myLocation['y'], myLocation['x']-1]
+    adjacent['right'] = [myLocation['y'], myLocation['x']+1]
+
+    return adjacent
+
+def isSafeSpace(adjacentCells, data):
+    potentialMove = {}
+    for direction, coordinate in adjacentCells.items():
+        safeSpace = True
+
+        # Check adjacentCells are within board
+        if coordinate[0] < 0 or coordinate[0] >= boardHeight: # checking if move 'up' or 'down' is a wall
+            safeSpace = False
+        elif coordinate[1] < 0 or coordinate[1] >= boardWidth: # checking if move 'left' or 'right' is a wall
+            safeSpace = False
+
+        # Check adjacentCells for another snake
+        for snake in data['board']['snakes']:
+            if safeSpace == False:
+                break
+
+            for snakePart in snake['body']:
+                if snakePart.values() == coordinate:
+                    safeSpace = False
+                    break
+
+        if safeSpace == True:
+            potentialMove[direction] = coordinate # Adds direction to potentialMove
+    print(potentialMove)
+    return potentialMove
+
+def getClosestFood(potentialMove, data):
+    closestFood = {}
+    closestDistance = 100
+
+    for berry in data['board']['food']:
+        distanceX = abs(myLocation['x'] - berry['x'])
+        distanceY = abs(myLocation['y'] - berry['y'])
+
+        actualDistance = math.sqrt( pow(distanceX, 2) + pow(distanceY, 2))
+
+        if actualDistance < closestDistance:
+            closestDistance = actualDistance
+            closestFood = berry
+
+    foodDirection = []
+    safeFoodDirection = []
+    if closestFood['y'] - myLocation['y'] < 0:
+        foodDirection.append('up')
+    elif closestFood['y'] - myLocation['y'] > 0:
+        foodDirection.append('down')
+
+    if closestFood['x'] - myLocation['x'] < 0:
+        foodDirection.append('left')
+    elif closestFood['x'] - myLocation['x'] > 0:
+        foodDirection.append('right')
+    print("Potential Move: ", potentialMove, "foodDirection: ", foodDirection)
+
+
+
+    for safeDirections in potentialMove.keys():
+        print(safeDirections)
+        if safeDirections in foodDirection:
+            safeFoodDirection.append(safeDirections)
+
+    return safeFoodDirection
+
 @bottle.post('/move')
 def move():
+    # Get request data
     data = bottle.request.json
+
 
     """
     TODO: Using the data from the endpoint request object, your
             snake AI must choose a direction to move in.
     """
     print(json.dumps(data))
-
-    directions = ['up', 'down', 'left', 'right']
-    direction = random.choice(directions)
+    global myLocation
+    myLocation = getMyLocation(data)
+    adjacentCells = getAdjacentCells(myLocation, data)
+    potentialMove = isSafeSpace(adjacentCells, data)
+    findFood = getClosestFood(potentialMove, data)
+#    huntFood = pursueFood(potentialMove, data)
+    print(findFood)
+    direction = random.choice(findFood)
+    if len(direction) == 0:
+        direction = ['up', 'down', 'left','right']
 
     return move_response(direction)
 
